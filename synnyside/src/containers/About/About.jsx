b52ad03625f95af91fe8d49a69b0314d4c0f55f4
Mootassam <mootassame@gmail.com>
2022-03-04 20:22:37 +0100

    change color of services

--- synnyside/src/containers/About/About.jsx
+++ synnyside/src/containers/About/About.jsx
@@ -34,34 +34,6 @@ const About = () => {
           </p>
         </div>
       </div>
-
-      <div className='app__about-cherry'>
-        <div className='app-about-cherry-blog'>
-          <div className='blog-text-cherry'>
-            <h3>Graphic Design</h3>
-            <span>
-              Great design makes you memorable . we Deliver artwork that
-              underscores your brand message and captures potential clients
-              attention .
-            </span>
-          </div>
-          <div className='blog-image-cherry'>
-            <img src={Cherry} alt='photo_cherry' />
-          </div>
-        </div>
-        <div className='app-about-orange-blog'>
-          <div className='blog-text-orange'>
-            <h3>Photography</h3>
-            <span>
-              Increase your credibility by getting the most stunning ,
-              high-quality photos that improve your business image .
-            </span>
-          </div>
-          <div className='blog-image-orange'>
-            <img src={Orange} alt='photo_orange' />
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
--- synnyside/src/containers/Services/Services.jsx
+++ synnyside/src/containers/Services/Services.jsx
@@ -1,47 +1,76 @@
 import React from "react";
-import { P1, P2, P3 } from "../../assets";
+import { Cherry, Orange, P1, P2, P3 } from "../../assets";
 import "./Services.scss";
 import { AppWrap } from "../../wrapper";
 const Services = () => {
   return (
-    <div className='app__testimonials'>
-      <div className='app__testimonials-text'>
-        <h3>
-          <b> Client Testimonials </b>
-        </h3>
-      </div>
-      <div className='app__testimonials-section'>
-        <div className='app__testimonials-items'>
-          <div className='app__testimonials-section p'>
-            <img src={P1} alt='' />
+    <>
+      <div className='app__about-cherry'>
+        <div className='app-about-cherry-blog'>
+          <div className='blog-text-cherry'>
+            <h3>Graphic Design</h3>
             <span>
-              We put trust Sunnysdie and they delivered, making sure our needs
-              were met and dealines were always hit .
+              Great design makes you memorable . we Deliver artwork that
+              underscores your brand message and captures potential clients
+              attention .
             </span>
-            <p className='author'>Emily R .</p>
-            <p className='post'>marketing Director</p>
           </div>
-          <div className='app__testimonials-section p'>
-            <img src={P2} alt='' />
-            <span>
-              We put trust Sunnysdie and they delivered, making sure our needs
-              were met and dealines were always hit .
-            </span>
-            <p className='author'>Emily R .</p>
-            <p className='post'>marketing Director</p>
+          <div className='blog-image-cherry'>
+            <img src={Cherry} alt='photo_cherry' />
           </div>
-          <div className='app__testimonials-section p'>
-            <img src={P3} alt='' />
+        </div>
+        <div className='app-about-orange-blog'>
+          <div className='blog-text-orange'>
+            <h3>Photography</h3>
             <span>
-              We put trust Sunnysdie and they delivered, making sure our needs
-              were met and dealines were always hit .
+              Increase your credibility by getting the most stunning ,
+              high-quality photos that improve your business image .
             </span>
-            <h3 className='author'>Emily R .</h3>
-            <p className='post'>marketing Director</p>
+          </div>
+          <div className='blog-image-orange'>
+            <img src={Orange} alt='photo_orange' />
           </div>
         </div>
       </div>
-    </div>
+      <div className='app__testimonials'>
+        <div className='app__testimonials-text'>
+          <h3>
+            <b> Client Testimonials </b>
+          </h3>
+        </div>
+        <div className='app__testimonials-section'>
+          <div className='app__testimonials-items'>
+            <div className='app__testimonials-section p'>
+              <img src={P1} alt='' />
+              <span>
+                We put trust Sunnysdie and they delivered, making sure our needs
+                were met and dealines were always hit .
+              </span>
+              <p className='author'>Emily R .</p>
+              <p className='post'>marketing Director</p>
+            </div>
+            <div className='app__testimonials-section p'>
+              <img src={P2} alt='' />
+              <span>
+                We put trust Sunnysdie and they delivered, making sure our needs
+                were met and dealines were always hit .
+              </span>
+              <p className='author'>Emily R .</p>
+              <p className='post'>marketing Director</p>
+            </div>
+            <div className='app__testimonials-section p'>
+              <img src={P3} alt='' />
+              <span>
+                We put trust Sunnysdie and they delivered, making sure our needs
+                were met and dealines were always hit .
+              </span>
+              <h3 className='author'>Emily R .</h3>
+              <p className='post'>marketing Director</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
